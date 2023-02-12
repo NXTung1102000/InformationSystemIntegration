f1bf8d140d8d7c2e927776947941fbd10cb56263
NXTung1102000 <nxtung1102000@gmail.com>
2023-02-12 18:29:48 +0700

    config redux for auth, config API

--- client-app/src/app/store.ts
+++ client-app/src/app/store.ts
@@ -1,11 +1,30 @@
-import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { configureStore, ThunkAction, Action, combineReducers } from "@reduxjs/toolkit";
 import counterReducer from "../pages/counter/counterSlice";
+import AuthSlice from "../pages/LogIn_Register/AuthSlice";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+};
+
+const reducer = combineReducers({
+  auth: persistReducer(authPersistConfig, AuthSlice),
+  counter: counterReducer,
+});
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
