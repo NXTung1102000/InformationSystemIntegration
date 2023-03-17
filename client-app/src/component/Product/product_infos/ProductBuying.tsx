0324d77133067f721119bb583532e4ca740e8300
Xuan Tung Trinh <xuantung.trinh.63@gmail.com>
2023-03-18 01:53:22 +0700

    flex by Grid for productBuying

--- client-app/src/component/Product/product_infos/ProductBuying.tsx
+++ client-app/src/component/Product/product_infos/ProductBuying.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {ProductModel} from "../../../model/ProductModel";
+import Grid from '@mui/material/Grid'
 
 import Price from './product_buying/Price'
 import Quantity from './product_buying/Quantity'
@@ -10,13 +11,15 @@ let productName: string = 'Macbook Air M1'
 
 export default function ProductBuying({product}: {product: ProductModel}) {
     return (
-        <div className='product-buying'>
-          <div>
+        <Grid container className='product-buying'>
+          <Grid item md={5} className='product-price-quantity-count'>
             <Price productPrice={product.price}/>
             <Quantity productCount={product.quantity}/>
             <Count/>
-          </div>
-          <AddToCart/>
-        </div>
+          </Grid>
+          <Grid item md={7}>
+            <AddToCart/>
+          </Grid>
+        </Grid>
     )
 }
